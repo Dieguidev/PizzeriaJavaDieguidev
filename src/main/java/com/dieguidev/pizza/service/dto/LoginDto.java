61dcf195366496e2b9deb7f76bd528de71ffd830
Dieguidev <diegogaraycullas@gmail.com>
2023-08-26 12:43:55 -0500

    creando filtros para verificar el jwt

---
+++ src/main/java/com/dieguidev/pizza/service/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.dieguidev.pizza.service.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String username;
+    private String password;
+}
--- src/main/java/com/dieguidev/pizza/web/config/JwtUtil.java
+++ src/main/java/com/dieguidev/pizza/web/config/JwtUtil.java
@@ -2,6 +2,8 @@ package com.dieguidev.pizza.web.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -11,12 +13,33 @@ import java.util.concurrent.TimeUnit;
 public class JwtUtil {
     private static String SECRET_KEY = "dieguidev_pizzeria";
     private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+
     public String create(String username) {
         return JWT.create()
                 .withSubject(username)
-                .withIssuer("dieguidev-pizzeria")
+                .withIssuer("dieguidev-pizza")
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
                 .sign(ALGORITHM);
+    }
+
+
+    //este metodo verifica que el JWT sea correcto y valido
+    public boolean isValid(String jwt) {
+        try {
+            JWT.require(ALGORITHM)
+                    .build()
+                    .verify(jwt);
+            return true;
+        }catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+    //este metodo trae el username a quien pertenece el jwt
+    public String getUserName(String jwt) {
+        return JWT.require(ALGORITHM)
+                .build()
+                .verify(jwt)
+                .getSubject();
     }
 }
